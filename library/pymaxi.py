97dcd01e2a9b0460a8cd73eb9b27100dc0b8df02
kathinkagerlinger <mail@familie-gerlinger.de>
2020-06-01 21:40:16 +0200

    read image data

--- library/pymaxi.py
+++ library/pymaxi.py
@@ -2,7 +2,7 @@
 Python Dictionary for getting the data out of the hdf files recorded with MAXI
 
 2020
-@author: dscran, KG ....
+@author: dscran, KG, RB
 """
 import h5py
 import numpy as np
@@ -36,3 +36,30 @@ def diode_scan(fname, entry_number, motor):
     diode = f[f'entry{entry_number:d}/measurement/diodeA'][()]
     motor_val = f[f'entry{entry_number:d}/measurement/{motor:s}'][()]
     return (diode, motor_val)
+
+
+def get_measurement(fname, entry_number, name):
+    '''
+    Function to get data of a specific entry and name, saved under measurement.
+    INPUT:  fname: string, path and name of the hdf file
+            entry_number: integer, number of the entry you want to check
+            name: string, name of the key you want to read. You can check it with measurement_info()
+    OUTPUT: list of the measured values
+    -----
+    author: KG 2020
+    '''
+    f = h5py.File(fname, 'r')
+    return f[f'entry{entry_number:d}/measurement/{name:s}'][()]
+
+
+def get_mte(fname, entry_number):
+    '''
+    Function to get data of a the camera of a specific entry.
+    INPUT:  fname: string, path and name of the hdf file
+            entry_number: integer, number of the entry you want to check
+    OUTPUT: array of the image data
+    -----
+    author: KG 2020
+    '''
+    f = h5py.File(fname, 'r')
+    return f[f'entry{entry_number:d}/measurement/mte'][()][0]
